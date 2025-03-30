e605b4c9416b39b0954331690b84e682a042dea1
Daniel Sinkpon <danielsinkpon@gmail.com>
2025-03-30 03:42:34 -0400

    Add Next-Fit Search option for finding blocks
    Create multiple modes of search for finding
    blocks, starting with First-Fit and then
    Next-Fit.
    
    Allow user to change to change the mode of
    search for finding blocks.

--- allocator.cpp
+++ allocator.cpp
@@ -13,9 +13,18 @@ struct Block {
     word_t data[1];
 };
 
+enum class SearchMode {
+    FirstFit,
+    NextFit,
+};
+
 static Block *heapStart = nullptr;
 
 static auto top = heapStart;
+
+static Block *searchStart = heapStart;
+
+static auto searchMode = SearchMode::FirstFit;
 
 inline size_t align(size_t n){
     return (n + sizeof(word_t) - 1) & ~(sizeof(word_t) - 1);
@@ -24,6 +33,24 @@ inline size_t align(size_t n){
 inline size_t allocSize(size_t size) {
     return size + sizeof(Block) - sizeof(std::declval<Block>().data);
 }
+
+void resetHeap(){
+    if (heapStart == nullptr){
+        return;
+    }
+
+    brk(heapStart);
+
+    heapStart = nullptr;
+    top = nullptr;
+    searchStart = nullptr;
+}
+
+void init(SearchMode mode) {
+    searchMode = mode;
+    resetHeap();
+}
+
 
 Block *firstFit(size_t size) {
     auto block = heapStart;
@@ -40,8 +67,51 @@ Block *firstFit(size_t size) {
     return nullptr;
 }
 
+Block *nextFit(size_t size) {
+
+    // start from previous successful position (if there is one)
+    auto block = searchStart;
+    while (block != nullptr) {
+        if (block->used || block->size < size){
+            block = block->next;
+            continue;
+        }
+        // found the block
+        // This is the current succesful position set searchStart to this block
+        searchStart = block;
+        return block;
+    }
+
+    // start again from the beginning
+    block = heapStart;
+
+    while (block != searchStart){
+        if (block->used || block->size < size){
+            block = block->next;
+            continue;
+        }
+
+        // found the block
+        // This is the current succesful position set searchStart to this block
+        searchStart = block;
+        return block;
+    }
+
+    // if we reach this point, we have not found a suitable block,
+    // even after a circular traversal fom searchStart
+    return nullptr;
+}
+
 Block *findBlock(size_t size) {
-    return firstFit(size);
+    if (searchMode == SearchMode::FirstFit) {
+        return firstFit(size);
+    }
+    else if (searchMode == SearchMode::NextFit) {
+        return nextFit(size);
+    }
+    else {
+        return nullptr;
+    }
 }
 
 Block *requestFromOS(size_t size){
@@ -90,23 +160,50 @@ void free(word_t *data) {
 }
 
 
+
+
 int main(int argc, char const *argv[]) {
     // std::cout << allocSize(12) << std::endl;
-    auto p1 = alloc(3);
-    auto p1b = getHeader(p1);
-    assert(p1b->size == sizeof(word_t));
 
-    auto p2 = alloc(8);
-    auto p2b = getHeader(p2);
-    assert(p2b->size == 8);
+    // auto p1 = alloc(3);
+    // auto p1b = getHeader(p1);
+    // assert(p1b->size == sizeof(word_t));
 
-    free(p2);
-    assert(p2b->used == false);
+    // auto p2 = alloc(8);
+    // auto p2b = getHeader(p2);
+    // assert(p2b->size == 8);
 
-    auto p3 = alloc(8);
-    auto p3b = getHeader(p3);
-    assert(p3b->size == 8);
-    assert(p3b == p2b);
+    // free(p2);
+    // assert(p2b->used == false);
+
+    // auto p3 = alloc(8);
+    // auto p3b = getHeader(p3);
+    // assert(p3b->size == 8);
+    // assert(p3b == p2b);
+
+    init(SearchMode::NextFit);
+    auto o1 = alloc(8);
+    auto o2 = alloc(8);
+    auto o3 = alloc(8);
+    
+    // [[8, 1], [8, 1], [8, 1], [16, 1], [16, 1]]
+    auto o4 = alloc(16);
+    auto o5 = alloc(16);
+    
+    // [[8, 1], [8, 1], [8, 1], [16, 0], [16, 0]]
+    free(o4);
+    free(o5);
+    
+    // [[8, 1], [8, 1], [8, 1], [16, 1], [16, 0]]
+    auto o6 = alloc(16);
+    
+    // Start position from o6:
+    assert(searchStart == getHeader(o6));
+    // [[8, 1], [8, 1], [8, 1], [16, 1], [16, 0]]
+    //                           ^ start here
+
+    alloc(16);
+
     puts("\nAll assertions passed!\n");
     return 0;
 }
